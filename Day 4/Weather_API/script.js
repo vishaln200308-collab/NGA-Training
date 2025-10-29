e3811cc6e2c74df9fd6374f05f958acdc1909dbf
Vishal N <vishaln200308@gmail.com>
2025-10-29 09:03:06 +0530

    Fourth commit

---
+++ Day 4/Weather_API/script.js
@@ -0,0 +1,9 @@
+// So if we want to implement a simple weather app that fetches weather data from a public API and displays it on the webpage, we can use the Fetch API along with async/await for better readability and error handling. 
+// with the help of strucural Module pattern we can organize our code better. lets see the steps to achieve this.
+// Step 1: Define the API endpoint and necessary DOM elements in seprate module named as weatherModule.js
+// Step 2: Create functions to fetch weather data from the API and display it on the webpage in script.js
+// Step 3: Use async/await to handle asynchronous operations and error handling in script.js
+// Step 4: Add event listeners to trigger the fetch operation when the user interacts with the webpage in script.js
+// Step 5: Test the application to ensure it works as expected after integrating it with the HTML file.
+// Step 6: Finally we can style the weather app using CSS to make it visually appealing.
+
